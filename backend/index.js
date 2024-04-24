3582bccc6f42b5606561137dfa7d55b879912db3
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-04-24 23:48:13 +0530

    mongoose connection and basic http connection succesfull

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-import express from 'express';
-import { PORT, mongodbURL } from './config.js';
-import mongoose from 'mongoose';
+import express from "express";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
 const app = express();
 
 //sending http request
-app.get('/', (request, response) => {
-    console.log(request)
-    return response.status(234).send('Kyou Hilla dala na')
-})
-
-//listener
-app.listen(PORT, () => {
-    console.log(`app is listening to port: ${PORT}`)
-})
+app.get("/", (request, response) => {
+  console.log(request);
+  return response.status(234).send("Kyou Hilla dala na");
+});
 
 //mongodb connection
-
-mongoose.connect(mongodbURL)
-.then(()=>{
-    console.log("Database Connection")
-}).catch((error)=>{
-    console.log(error)
-})
+mongoose
+  .connect(mongodbURL)
+  .then(() => {
+    console.log("Database Connection Succesfull");
+    //listener
+    app.listen(PORT, () => {
+      console.log(`app is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
