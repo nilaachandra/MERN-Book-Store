f9befea8377edde1b721baa9bccf6f8f112a46b2
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-04-26 23:17:49 +0530

    database schema created

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,41 @@
 import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/model.js";
+
 const app = express();
+
+//middleware to parse requset body
+app.use(express.json())
 
 //sending http request
 app.get("/", (request, response) => {
   console.log(request);
   return response.status(234).send("Kyou Hilla dala na");
 });
+
+// Route to save a new book
+app.post('/books', async (request, response) => {
+  try {
+    if(!request.body.title || !request.body.author || !request.body.publisher || !request.body.publishYear)
+    {
+      return response.status(400).send({
+        message: 'send all required fields'
+      });
+    }
+    const newBook = {
+      title: request.body.title,
+      author: request.body.author,
+      publisher: request.body.publisher,
+      publishYear: request.body.publishYear
+    }
+    const book = await Book.create(newBook)
+    return response.status(201).send(book)
+  } catch (error) {
+    console.log(error.message)
+    response.status(500).send({message: error.message})
+  }
+})
 
 //mongodb connection
 mongoose
---
+++ backend/models/model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"
+
+const bookSchema = mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: String,
+            required: true,
+        },
+        publisher: {
+            type: String,
+            required: true,
+        },
+        publishYear: {
+            type: Number,
+            required: true,
+        },
+    },
+    {
+        timestamps:true,
+    }
+)
+
+
+export const Book = mongoose.model('Book', bookSchema)
