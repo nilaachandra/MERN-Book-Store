b481c3582cf38d2b2d4b634c6832e031a3deb3ca
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-05-05 00:52:23 +0530

    toaster added

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -1,22 +1,15 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Loader from "./Loader";
-import { Link } from "react-router-dom";
 import { useQuery, useMutation, QueryClient } from "@tanstack/react-query";
-import {
-  RiAddBoxFill,
-  RiDeleteBin2Fill,
-  RiEdit2Fill,
-  RiFileAddLine,
-  RiHeartFill,
-  RiInformation2Fill,
-} from "@remixicon/react";
-import {Toaster, toast } from 'sonner'
+import { RiFileAddLine } from "@remixicon/react";
+import { Toaster, toast } from "sonner";
+
 const Homepage = () => {
   const queryClient = new QueryClient();
 
   // Using tanStack React Query to fetch data
-  const { data, isLoading, isError, refetch } = useQuery({
+  const { data, isLoading, onSuccess, refetch } = useQuery({
     queryKey: ["data"],
     queryFn: async () => {
       try {
@@ -46,6 +39,13 @@ const Homepage = () => {
   const [editedPublisher, setEditedPublisher] = useState("");
   const [editedPublishYear, setEditedPublishYear] = useState("");
 
+  // State for managing add modal
+  const [addModalOpen, setAddModalOpen] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+  const [newAuthor, setNewAuthor] = useState("");
+  const [newPublisher, setNewPublisher] = useState("");
+  const [newPublishYear, setNewPublishYear] = useState("");
+
   // Function to handle edit button click
   const handleEditClick = (book) => {
     setEditingBook(book);
@@ -69,20 +69,62 @@ const Homepage = () => {
       setEditingBook(null);
       refetch();
       setEditModalOpen(false);
-      toast.success( 'Book Update Successfull', { position: 'top-center', richColors: true, duration: 2000 });
-
+      toast.success("Book Update Successfull", {
+        position: "top-center",
+        richColors: true,
+        duration: 2000,
+      });
     } catch (error) {
       console.error("Error updating book:", error);
     }
   };
 
   // Function to handle delete button click
-  const handleDelete = async (id,title) => {
+  const handleDelete = async (id, title) => {
     try {
       await deleteBook.mutateAsync(id);
-      toast.error(`${title} deleted`, { position: 'top-center', richColors: true, duration: 2000 });
+      toast.error(`${title} deleted`, {
+        position: "top-center",
+        richColors: true,
+        duration: 2000,
+      });
     } catch (error) {
       console.error("Error deleting book:", error);
+    }
+  };
+
+  // Function to handle add book button click
+  const handleAddBookClick = () => {
+    setAddModalOpen(true);
+  };
+
+  // Function to handle save new book
+  const handleSaveNewBook = async () => {
+    try {
+      await axios.post(`http://localhost:5555/books`, {
+        title: newTitle,
+        author: newAuthor,
+        publisher: newPublisher,
+        publishYear: newPublishYear,
+      });
+
+      // Clear the input fields
+      setNewTitle("");
+      setNewAuthor("");
+      setNewPublisher("");
+      setNewPublishYear("");
+      // Close the add modal
+      setAddModalOpen(false);
+      toast.success(`${newTitle} Added Successfully`, {
+        position: "top-center",
+        richColors: true,
+        duration: 2000,
+      });
+
+      // Refetch data to update the book list
+      refetch();
+    } catch (error) {
+      console.error("Error adding new book:", error);
     }
   };
 
@@ -90,12 +132,12 @@ const Homepage = () => {
     <div className="p-4 w-full justify-between items-center">
       <div className=" w-full flex justify-between items-center">
         <h1 className="text-3xl my-8">Books List</h1>
-        <Link
-          to="books/create"
+        <button
+          onClick={handleAddBookClick}
           className="flex gap-3 justify-center items-center"
         >
           Add Books <RiFileAddLine size={48} />
-        </Link>
+        </button>
       </div>
       {isLoading ? (
         <Loader />
@@ -122,8 +164,18 @@ const Homepage = () => {
                 <td className="p-2">
                   <div className="flex items-center gap-x-4">
                     {/* Pass book data to handleEditClick */}
-                    <button onClick={() => handleEditClick(item)} className="bg-green-700 px-4 py-1 rounded-md font-bold text-white hover:opacity-80 transition-all duration-300">Edit</button>
-                    <button onClick={() => handleDelete(item._id, item.title)} className="bg-red-700 px-4 py-1 rounded-md font-bold text-white hover:opacity-80 transition-all duration-300">Delete</button>
+                    <button
+                      onClick={() => handleEditClick(item)}
+                      className="bg-green-700 px-4 py-1 rounded-md font-bold text-white hover:opacity-80 transition-all duration-300"
+                    >
+                      Edit
+                    </button>
+                    <button
+                      onClick={() => handleDelete(item._id, item.title)}
+                      className="bg-red-700 px-4 py-1 rounded-md font-bold text-white hover:opacity-80 transition-all duration-300"
+                    >
+                      Delete
+                    </button>
                   </div>
                 </td>
               </tr>
@@ -166,15 +218,75 @@ const Homepage = () => {
               className="block w-full border border-gray-300 rounded-md px-4 py-2 mb-2"
             />
             <div className="flex justify-end">
-              <button onClick={handleSave} className="bg-green-500 text-white font-bold rounded-md px-4 py-2 mr-2 hover:opacity-80 transition-all duration-300">Save</button>
-              <button onClick={() => setEditModalOpen(false)} className="bg-gray-300 text-gray-800 rounded-md px-4 py-2 hover:opacity-80 transition-all duration-300">Cancel</button>
+              <button
+                onClick={handleSave}
+                className="bg-green-500 text-white font-bold rounded-md px-4 py-2 mr-2 hover:opacity-80 transition-all duration-300"
+              >
+                Save
+              </button>
+              <button
+                onClick={() => setEditModalOpen(false)}
+                className="bg-gray-300 text-gray-800 rounded-md px-4 py-2 hover:opacity-80 transition-all duration-300"
+              >
+                Cancel
+              </button>
             </div>
           </div>
         </div>
       )}
 
- <Toaster position="top-center" richColors duration={2000}/>
+      {/* Add modal */}
+      {addModalOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-500 bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg w-[30%]">
+            <h2 className="text-xl font-bold mb-4">Add Book</h2>
+            <label>Title:</label>
+            <input
+              type="text"
+              value={newTitle}
+              onChange={(e) => setNewTitle(e.target.value)}
+              className="block w-full border border-gray-300 rounded-md px-4 py-2 mb-2"
+            />
+            <label>Author:</label>
+            <input
+              type="text"
+              value={newAuthor}
+              onChange={(e) => setNewAuthor(e.target.value)}
+              className="block w-full border border-gray-300 rounded-md px-4 py-2 mb-2"
+            />
+            <label>Publisher:</label>
+            <input
+              type="text"
+              value={newPublisher}
+              onChange={(e) => setNewPublisher(e.target.value)}
+              className="block w-full border border-gray-300 rounded-md px-4 py-2 mb-2"
+            />
+            <label>Publish Year:</label>
+            <input
+              type="text"
+              value={newPublishYear}
+              onChange={(e) => setNewPublishYear(e.target.value)}
+              className="block w-full border border-gray-300 rounded-md px-4 py-2 mb-2"
+            />
+            <div className="flex justify-end">
+              <button
+                onClick={handleSaveNewBook}
+                className="bg-green-500 text-white font-bold rounded-md px-4 py-2 mr-2 hover:opacity-80 transition-all duration-300"
+              >
+                Add
+              </button>
+              <button
+                onClick={() => setAddModalOpen(false)}
+                className="bg-gray-300 text-gray-800 rounded-md px-4 py-2 hover:opacity-80 transition-all duration-300"
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
 
+      <Toaster position="top-center" richColors duration={2000} />
     </div>
   );
 };
