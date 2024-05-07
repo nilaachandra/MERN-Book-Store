9cbe6606ea3525fd886cb52a29aac178d8f57b58
Nila Chandra Laishram <nilalaishram@gmail.com>
2024-05-07 23:07:19 +0530

    bug fix

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,3 @@
+
 export const PORT = 5555;
 export const mongodbURL = 'mongodb+srv://nilalaishram:HALAMADRID14@bookstoremern.a2tpp4y.mongodb.net/?retryWrites=true&w=majority&appName=BookStoreMERN'
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from './routes/booksRoute.js'
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
 
